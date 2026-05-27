85812faad6f32824e6d1d75195376f26bbb9dd61
Lorenzo6900 <lorenzolaniado@gmail.com>
2026-05-27 13:48:34 -0300

    codigo_parte2

---
+++ Assets/scripts/Recolector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Recolector : MonoBehaviour
+{
+    public int puntos = 0;
+
+    public TextMeshProUGUI textoPuntos;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coleccionable"))
+        {
+            Destroy(other.gameObject);
+
+            puntos++;
+
+            textoPuntos.text = "Puntos: " + puntos;
+        }
+    }
+}
