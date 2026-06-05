a5ac9d37949fba98cac458abc58c3d45650ea385
Joaquin Schopflocher <joacoschop@gmail.com>
2026-06-05 09:07:09 -0300

    Clase 4: Pantallas de fin de juego y sistema de reinicio operativos

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 public TextMeshProUGUI textoCronometro;
 
-    private float tiempo = 0f;
+    private float tiempo = 60f;
  public bool corriendo = true;
 
     void Update()
     {
-       
-        if (corriendo)
-        {
-            tiempo += Time.deltaTime;
-        }
-
-        textoCronometro.text = tiempo.ToString("F2");
+      if (Input.GetKeyDown(KeyCode.R))
+    {
+        tiempo = 20f;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    if (corriendo)
+    {
+        tiempo -= Time.deltaTime;
+    }
+
+    textoCronometro.text = tiempo.ToString("F2");
+}
 
     public void pararT(){
         corriendo = false;
--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -1,24 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI textoCronometro;
     public TextMeshProUGUI textoPuntos;
+    public TextMeshProUGUI textoFinal;
+    public TextMeshProUGUI textoSub;
+    public Image panel;
 
-    private float tiempo = 0f;
+    private float tiempo = 30f;
     public bool corriendo = true;
 
     void Update()
     {
         if (corriendo)
         {
-            tiempo += Time.deltaTime;
+            tiempo -= Time.deltaTime;
         }
 
         textoCronometro.text = tiempo.ToString("F2");
+        if(tiempo <= 0f){
+            textoPuntos.text = "";
+            textoFinal.text = "Perdiste";
+            textoSub.text = "Apreta R para volver a empezar.";
+            Color c = Color.red;
+            c.a = 0.8f;
+            panel.color = c;
+            pararT();
+            Time.timeScale = 0f;
+            tiempo = 30f;
+        }
     }
 
     public void pararT()
@@ -34,8 +49,14 @@ public class UIManager : MonoBehaviour
         }
         else
         {
-            textoPuntos.text = "Ganaste!";
+            textoPuntos.text = "";
+            textoFinal.text = "Ganaste!";
+            textoSub.text = "Apreta R para volver a empezar.";
+            Color c = Color.green;
+            c.a = 0.8f;
+            panel.color = c;
             pararT();
+            Time.timeScale = 0f;
         }
     }
 }
