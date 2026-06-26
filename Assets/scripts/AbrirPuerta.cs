6b9952548bac4f228f3969064a6fa10b9bc6b6d0
Joaquin Schopflocher <joacoschop@gmail.com>
2026-06-26 09:22:58 -0300

    terminado

---
+++ Assets/scripts/AbrirPuerta.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirPuerta : MonoBehaviour
+{
+    public bool estaAbierta = false;
+    public float gradosDeApertura = 90f;
+    public float velocidad = 2f;
+
+    private Quaternion rotacionCerrada;
+    private Quaternion rotacionAbierta;
+
+    void Start()
+    {
+        // Guarda la rotación inicial de la puerta cerrada
+        rotacionCerrada = transform.rotation;
+        // Calcula la rotación que tendrá al abrirse
+        rotacionAbierta = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + gradosDeApertura, transform.eulerAngles.z);
+    }
+
+    void Update()
+    {
+        // Mueve suavemente la puerta hacia su estado actual
+        if (estaAbierta)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotacionAbierta, Time.deltaTime * velocidad);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotacionCerrada, Time.deltaTime * velocidad);
+        }
+    }
+
+    // Método que llamaremos desde el jugador para abrir/cerrar
+    public void Interactuar()
+    {
+        estaAbierta = !estaAbierta;
+    }
+}
+
+
+
---
+++ Assets/scripts/InteractionPuerta.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPuerta : MonoBehaviour
+{
+    public float distanciaDeInteraccion = 5f;
+    public LayerMask capaDeLaPuerta; // Útil para optimizar (opcional)
+
+    void Update()
+    {
+        // Crea un rayo desde el centro de la cámara hacia adelante
+        Ray rayo = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        // Dibuja el rayo en la pestaña Scene para que puedas verlo (opcional)
+        Debug.DrawRay(transform.position, transform.forward * distanciaDeInteraccion, Color.red);
+
+        // Si el rayo golpea un objeto y presionas la tecla "E"
+        if (Physics.Raycast(rayo, out hit, distanciaDeInteraccion))
+        {
+            // Verificamos si lo que miramos tiene el script AbridorPuerta
+            AbrirPuerta puerta = hit.collider.GetComponentInParent<AbrirPuerta>();
+
+            if (puerta != null)
+            {
+                // Aquí puedes mostrar un texto en pantalla: "Presiona E para abrir"
+                
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    puerta.Interactuar();
+                }
+            }
+        }
+    }
+}
+
+
